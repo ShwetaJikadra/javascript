a4271ad0e29690c7f150db919d01fa156b0d6047
ShwetaJikadra <setujikadra@gmail.com>
2023-08-29 17:35:31 +0530

    29/08/2023

---
+++ datetime.js
@@ -0,0 +1,31 @@
+// let a=new Date();
+// console.log(a)
+// let a1=new Date(2023)
+// console.log(a1)
+// let a2=new Date(2023,7)
+// console.log(a2)
+// let a3=new Date(2023,7,6);
+// console.log(a3)
+// let a4=new Date(2023,7,7,30,777)
+// console.log(a4)
+
+// let x=a.getFullYear();
+// console.log(x)
+// console.log(a.getDate())
+// console.log(a.getDay())
+
+// console.log(a.getMinutes())
+// console.log(a.getMonth())
+// console.log(a.getMilliseconds())
+// console.log(a.getHours())
+// console.log(a.getTime())
+// console.log(a.getMonth())
+// console.log(a.getTimezoneOffset())
+// console.log(a.getUTCDate())
+// console.log(a.getUTCDay())
+// console.log(a.getUTCMilliseconds())
+// console.log(a.toLocaleTimeString())
+var t;
+let a1=new Date();
+t=a1.setFullYear(2000)
+console.log(t)
